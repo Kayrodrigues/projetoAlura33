b51540e2278881ac80b41ca70f1655404f37771f
Kayrodrigues <137062787+Kayrodrigues@users.noreply.github.com>
2024-09-24 08:26:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro homem a pisar na lua',
+    'Neil Armstrong'
+)
+
+criaCartao(
+    'História',
+    'Quem ganhou a corrida espacial da Guerra Fria?',
+    'Estados Unidos'
+)
+
+criaCartao(
+    'História',
+    'Qual o esporte mais práticado no Brasil?',
+    'Futebol'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'Tokyo'
+)
+
+criaCartao(
+    'Geografia',
+    'Bebida típica na Alemanha?',
+    'Steinhaeger'
+)
+
+criaCartao(
+    'Anatomia',
+    'Maior osso do corpo humano?',
+    'Femur'
+)
+
+criaCartao(
+    'Biologia',
+    'Nome dado a veias extremamentes finas',
+    'Capilares'
+)
+
+criaCartao(
+    'Biologia',
+    'O que o fígado produz?',
+    'Bile'
+)
+
+criaCartao(
+    'Biologia',
+    'Nome dos orgãos que podem produzir cristais e pedras',
+    'Rins e Vesícula'
+)
+
+criaCartao(
+    'Biologia',
+    'Quem filtra o sangue?',
+    'Rins'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a comida típica do Brasil?',
+    'Feijoada'
+)
+
+criaCartao(
+    'História',
+    'Qual dia é comemorado o Dia da Consciência Negra?',
+    '20 de novembro'
+)
